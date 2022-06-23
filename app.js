8f5d4e7196c04ffb8d185dbe65bde2a42d77fb0b
dyn4casie <dyn4casie@gmail.com>
2022-06-23 23:17:19 +0100

    modified app.js

--- app.js
+++ app.js
@@ -5,4 +5,11 @@
     
     //document.getElementById("here").innerHTML = first+" "+second;
 //}
-alert("Name: Adedoyin Adetunji \nHeight: 5ft 9In \nCountry: Nigeria")
+//alert("Name: Adedoyin Adetunji \nHeight: 5ft 9In \nCountry: Nigeria")
+
+
+document.write("Name: Adedoyin Adetunji");
+document.write("<br>");
+document.write("Height: 5ft 9in");
+document.write("<br>");
+document.write("Country: Nigeria");
