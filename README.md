a5a886889ff0dfbee7a681a15653b47c0b6a8903
dyn4casie <dyn4casie@gmail.com>
2022-06-23 22:21:09 +0100

    modified readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Javascript-zuri
+# Write a javascript code that prints out your: 
+## Name 
+## Height
+## Country 
+# On the screen
---
+++ app.js
@@ -0,0 +1,8 @@
+//myFunction = function() {
+  //  var first = document.getElementById("fullname").value;
+    //var second = document.getElementById("height").value;
+    
+    
+    //document.getElementById("here").innerHTML = first+" "+second;
+//}
+alert("Name: Adedoyin Adetunji \nHeight: 5ft 9In \nCountry: Nigeria")
